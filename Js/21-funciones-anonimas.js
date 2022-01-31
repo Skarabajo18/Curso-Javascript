56c35af7a9da60a76adeaa8a2771a4efd3c9956e
Skarabajo18 <felipesegovia14@gmail.com>
2022-01-31 03:38:44 -0300

    js

--- Js/21-funciones-anonimas.js
+++ Js/21-funciones-anonimas.js
@@ -3,6 +3,26 @@
 // Funciones anonimas
 // Es una funcion que no tiene nombre
 
+
+/*
 var pelicula = function(nombre){
     return "La pelicua es: " + nombre;
 }
+*/
+
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
+    var sumar = numero1 + numero2;
+    sumaYmuestra(sumar);
+    sumaPorDos(sumar);
+    return  sumar;
+}
+
+sumame(5, 7,function(dato){
+    console.log("La suma es:", dato);
+},
+function(dato){
+    console.log("La suma por dos es: ",(dato*2));
+});
+
+
+
